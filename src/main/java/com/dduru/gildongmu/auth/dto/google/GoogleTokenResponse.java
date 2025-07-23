d4b10dab10530d731a3032ab744043c799ca5b7b
jaejoong0529 <wcn07003@naver.com>
2025-07-23 17:46:53 +0900

    [#8] feat(auth): Google 관련 DTO 추가

---
+++ src/main/java/com/dduru/gildongmu/auth/dto/google/GoogleTokenResponse.java
@@ -0,0 +1,24 @@
+package com.dduru.gildongmu.auth.dto.google;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GoogleTokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("scope")
+    private String scope;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
