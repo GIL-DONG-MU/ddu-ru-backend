870a4308b6348e85cf4738bf5363e0902ecb8c2c
jaejoong0529 <wcn07003@naver.com>
2025-07-29 17:53:21 +0900

    [#13] feat(post): 게시글 수정(Patch) API 엔드포인트 추가

--- src/main/java/com/dduru/gildongmu/post/controller/PostController.java
+++ src/main/java/com/dduru/gildongmu/post/controller/PostController.java
@@ -3,15 +3,14 @@ package com.dduru.gildongmu.post.controller;
 import com.dduru.gildongmu.common.annotation.CurrentUser;
 import com.dduru.gildongmu.post.dto.PostCreateRequest;
 import com.dduru.gildongmu.post.dto.PostCreateResponse;
+import com.dduru.gildongmu.post.dto.PostUpdateRequest;
+import com.dduru.gildongmu.post.dto.PostUpdateResponse;
 import com.dduru.gildongmu.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,14 @@ public class PostController {
             @Valid @RequestBody PostCreateRequest request) {
         PostCreateResponse response = postService.create(userId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PatchMapping("/{postId}")
+    public ResponseEntity<PostUpdateResponse> updatePost(
+            @PathVariable Long postId,
+            @CurrentUser Long userId,
+            @Valid @RequestBody PostUpdateRequest request){
+        PostUpdateResponse response = postService.update(postId, userId, request);
+        return ResponseEntity.ok(response);
     }
 }
