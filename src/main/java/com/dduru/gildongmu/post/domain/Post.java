6d2f64ef2ede7dc1d3a2577e3386b8fef98c1ad7
jaejoong0529 <wcn07003@naver.com>
2025-07-29 17:49:06 +0900

    [#13] feat(post): updatePost 메서드 추가 및 불필요 메서드 제거

--- src/main/java/com/dduru/gildongmu/post/domain/Post.java
+++ src/main/java/com/dduru/gildongmu/post/domain/Post.java
@@ -6,8 +6,8 @@ import com.dduru.gildongmu.auth.enums.Gender;
 import com.dduru.gildongmu.common.entity.BaseTimeEntity;
 import com.dduru.gildongmu.post.enums.Destination;
 import com.dduru.gildongmu.post.enums.PostStatus;
-import com.dduru.gildongmu.post.exception.RecruitmentEmptyException;
-import com.dduru.gildongmu.post.exception.RecruitmentFullException;
+import com.dduru.gildongmu.post.exception.InvalidRecruitCapacityException;
+import com.dduru.gildongmu.post.exception.TravelAlreadyStartedException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -140,6 +140,29 @@ public class Post extends BaseTimeEntity {
                 .build();
     }
 
+    public void updatePost(Destination destination, String title, String content,
+                           LocalDate startDate, LocalDate endDate, Integer recruitCapacity,
+                           LocalDate recruitDeadline, Gender preferredGender,AgeRange preferredAgeMin, AgeRange preferredAgeMax,
+                           Integer budgetMin, Integer budgetMax, String photoUrls, String tags){
+        validateUpdatePermission();
+
+        this.destination = destination;
+        this.title = title;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.recruitDeadline = recruitDeadline;
+        this.preferredGender = preferredGender != null ? preferredGender : Gender.U;
+        this.preferredAgeMin = preferredAgeMin;
+        this.preferredAgeMax = preferredAgeMax;
+        this.budgetMin = budgetMin;
+        this.budgetMax = budgetMax;
+        this.photoUrls = photoUrls;
+        this.tags = tags;
+
+        updateRecruitCapacity(recruitCapacity);
+    }
+
     public boolean isRecruitmentClosed() {
         return LocalDate.now().isAfter(recruitDeadline);
     }
@@ -152,21 +175,22 @@ public class Post extends BaseTimeEntity {
         return LocalDate.now().isAfter(endDate);
     }
 
-    public void increaseRecruitCount() {
-        if (this.recruitCount >= this.recruitCapacity) {
-            throw new RecruitmentFullException(this.getId());
+    private void updateRecruitCapacity(Integer newCapacity) {
+        if (newCapacity < this.recruitCount) {
+            throw new InvalidRecruitCapacityException(
+                    String.format("모집 인원은 현재 신청자 수(%d명)보다 적을 수 없습니다. 요청된 인원: %d명",
+                            this.recruitCount, newCapacity)
+            );
         }
-        this.recruitCount++;
+        this.recruitCapacity = newCapacity;
     }
-
-    public void decreaseRecruitCount() {
-        if (this.recruitCount <= 0) {
-            throw new RecruitmentEmptyException(this.getId());
+    private void validateUpdatePermission() {
+        if (isTravelStarted()) {
+            throw new TravelAlreadyStartedException("여행이 시작된 게시글은 수정할 수 없습니다");
         }
-        this.recruitCount--;
-    }
 
-    public boolean isRecruitmentFull() {
-        return this.recruitCount >= this.recruitCapacity;
+        if (isTravelEnded()) {
+            throw new TravelAlreadyStartedException("여행이 종료된 게시글은 수정할 수 없습니다");
+        }
     }
 }
