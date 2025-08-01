e4ea9bbb2a1fd2d069e8e6a6bd76ccbcb5625fa6
jaejoong0529 <wcn07003@naver.com>
2025-08-01 16:20:46 +0900

    [#22] chore(post): RequestMapping 수정

--- src/main/java/com/dduru/gildongmu/post/controller/PostController.java
+++ src/main/java/com/dduru/gildongmu/post/controller/PostController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/posts")
+@RequestMapping("/api/v1/posts")
 public class PostController {
     private final PostService postService;
 
