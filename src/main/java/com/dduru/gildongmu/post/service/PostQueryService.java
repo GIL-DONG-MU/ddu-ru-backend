5286034d272c14a1b0e0e853d7affb3423d02056
jaejoong0529 <wcn07003@naver.com>
2025-08-06 20:08:13 +0900

    [#16] feat(post): 게시글 상세조회 및 조회수 증가 기능 구현

--- src/main/java/com/dduru/gildongmu/post/service/PostQueryService.java
+++ src/main/java/com/dduru/gildongmu/post/service/PostQueryService.java
@@ -2,6 +2,7 @@ package com.dduru.gildongmu.post.service;
 
 import com.dduru.gildongmu.common.util.JsonConverter;
 import com.dduru.gildongmu.post.domain.Post;
+import com.dduru.gildongmu.post.dto.PostDetailResponse;
 import com.dduru.gildongmu.post.dto.PostListRequest;
 import com.dduru.gildongmu.post.dto.PostListResponse;
 import com.dduru.gildongmu.post.dto.PostSummaryResponse;
@@ -44,14 +45,18 @@ public class PostQueryService {
     }
 
     @Transactional
-    public void increaseViewCount(Long postId) {
-        log.debug("조회수 증가 - postId: {}", postId);
+    public PostDetailResponse getPostDetailWithViewCount(Long postId) {
+        log.debug("게시글 상세 조회 및 조회수 증가 시작 - postId: {}", postId);
 
-        if (!postRepository.existsById(postId)) {
-            throw new PostNotFoundException("게시글을 찾을 수 없습니다. postId: " + postId);
-        }
+        Post post = postRepository.findActiveById(postId)
+                .orElseThrow(() -> new PostNotFoundException("게시글을 찾을 수 없습니다. postId: " + postId));
 
         postRepository.incrementViewCount(postId);
-        log.info("조회수 증가 완료 - postId: {}", postId);
+
+        PostDetailResponse response = PostDetailResponse.from(post, jsonConverter);
+
+        log.info("게시글 상세 조회 및 조회수 증가 완료 - postId: {}, title: {}", postId, response.title());
+
+        return response;
     }
 }
