b8a25611c176522bc3058a277d7ada555db4883a
jaejoong0529 <wcn07003@naver.com>
2025-08-20 16:38:54 +0900

    [#69] refactor(destination):destination 고유 아이디로 존재 여부 확인 로직을 repository로 이동

--- src/main/java/com/dduru/gildongmu/destination/repository/DestinationRepository.java
+++ src/main/java/com/dduru/gildongmu/destination/repository/DestinationRepository.java
@@ -1,7 +1,12 @@
 package com.dduru.gildongmu.destination.repository;
 
 import com.dduru.gildongmu.destination.domain.Destination;
+import com.dduru.gildongmu.post.exception.DestinationNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DestinationRepository extends JpaRepository<Destination, Long> {
+    default Destination getByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> DestinationNotFoundException.of(id));
+    }
 }
--- src/main/java/com/dduru/gildongmu/post/exception/DestinationNotFoundException.java
+++ src/main/java/com/dduru/gildongmu/post/exception/DestinationNotFoundException.java
@@ -11,4 +11,8 @@ public class DestinationNotFoundException extends BusinessException {
     public DestinationNotFoundException(String message) {
         super(ErrorCode.DESTINATION_NOT_FOUND, message);
     }
+
+    public static DestinationNotFoundException of(Long destinationId) {
+        return new DestinationNotFoundException("여행지를 찾을 수 없습니다. destinationId=" + destinationId);
+    }
 }
