605c23898dc87b1aae76ec23c12a28f42b9c1a85
jaejoong0529 <wcn07003@naver.com>
2025-08-20 16:41:53 +0900

    [#69] refactor(post):post 고유 아이디로 존재 여부 확인 로직을 repository로 이동

--- src/main/java/com/dduru/gildongmu/post/repository/PostRepository.java
+++ src/main/java/com/dduru/gildongmu/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.dduru.gildongmu.post.repository;
 
 import com.dduru.gildongmu.post.domain.Post;
+import com.dduru.gildongmu.post.exception.PostNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,9 @@ public interface PostRepository extends JpaRepository<Post, Long>,PostRepository
     @Modifying
     @Query("UPDATE Post p SET p.viewCount = p.viewCount + 1 WHERE p.id = :postId")
     void incrementViewCount(@Param("postId") Long postId);
+
+    default Post getActiveByIdOrThrow(Long id) {
+        return findActiveById(id)
+                .orElseThrow(() -> PostNotFoundException.of(id));
+    }
 }
