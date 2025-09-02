0459e1623899f38b867d80f8ce906eeeec2ff642
shinheekim <dev.shinheekim@gmail.com>
2025-09-02 17:15:09 +0900

    [#72] chore(config): server 설정 삭제

--- src/main/java/com/dduru/gildongmu/config/SwaggerConfig.java
+++ src/main/java/com/dduru/gildongmu/config/SwaggerConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,7 +18,6 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(getInfo())
-                .servers(getServers())
                 .addSecurityItem(getSecurityRequirement())
                 .components(getComponents());
     }
@@ -39,13 +35,6 @@ public class SwaggerConfig {
 
     private SecurityRequirement getSecurityRequirement() {
         return new SecurityRequirement().addList(SCHEME_NAME);
-    }
-
-    private List<Server> getServers() {
-        return List.of(
-                new Server().url("http://localhost:8080").description("개발 환경"),
-                new Server().url("https://dduru.app").description("운영 환경")
-        );
     }
 
     private Components getComponents() {
