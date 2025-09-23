60327b14d5d3fbe810431dee89dfe76ec2dee7ae
jaejoong0529 <wcn07003@naver.com>
2025-09-23 16:51:19 +0900

    [#85] chore(post): ErrorCode 수정

--- src/main/java/com/dduru/gildongmu/post/exception/InvalidPostStatusException.java
+++ src/main/java/com/dduru/gildongmu/post/exception/InvalidPostStatusException.java
@@ -8,6 +8,6 @@ public class InvalidPostStatusException extends BusinessException {
         super(ErrorCode.INVALID_POST_STATUS, "모집이 완료된 게시글은 모집 상태를 변경할 수 없습니다.");
     }
     public InvalidPostStatusException(String message) {
-        super(ErrorCode.INVALID_POST_DATE, message);
+        super(ErrorCode.INVALID_POST_STATUS, message);
     }
 }
