2d2c3eb76d7fc2257c52aecabff176fe329d4090
shinheekim <dev.shinheekim@gmail.com>
2025-12-09 18:29:46 +0900

    [#111] style(api): ApiResult에서 각 상태 코드에 맞는 메서드 구현(data가 null일 시 Json에 표시 X)

--- src/main/java/com/dduru/gildongmu/common/dto/ApiResult.java
+++ src/main/java/com/dduru/gildongmu/common/dto/ApiResult.java
@@ -1,14 +1,31 @@
 package com.dduru.gildongmu.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.http.HttpStatus;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResult<T>(
-        Integer status,
+        int status,
         T data
 ) {
-    public static <T> ApiResult<T> success(T data) {
-        return new ApiResult<>(200, data);
+    public static <T> ApiResult<T> ok(T data) {
+        return new ApiResult<>(HttpStatus.OK.value(), data);
     }
 
-    public static <T> ApiResult<T> success(Integer status, T data) {
-        return new ApiResult<>(status, data);
+    public static <T> ApiResult<T> created(T data) {
+        return new ApiResult<>(HttpStatus.CREATED.value(), data);
+    }
+
+    public static ApiResult<Void> noContent() {
+        return new ApiResult<>(HttpStatus.NO_CONTENT.value(), null);
+    }
+
+    /* 커스텀 HTTP 상태 코드와 데이터 */
+    public static <T> ApiResult<T> of(HttpStatus status, T data) {
+        return new ApiResult<>(status.value(), data);
+    }
+
+    public static <T> ApiResult<T> of(int statusCode, T data) {
+        return new ApiResult<>(statusCode, data);
     }
 }
