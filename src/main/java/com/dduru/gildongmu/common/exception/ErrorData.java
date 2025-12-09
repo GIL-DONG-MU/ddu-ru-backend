6556d4403a48d717d355e806cb3cc846e64074a9
shinheekim <dev.shinheekim@gmail.com>
2025-12-09 19:27:02 +0900

    [#111] chore(api): @JsonInclude로 ErrorData null인 값 Json에 표시 X

--- src/main/java/com/dduru/gildongmu/common/exception/ErrorData.java
+++ src/main/java/com/dduru/gildongmu/common/exception/ErrorData.java
@@ -1,5 +1,8 @@
 package com.dduru.gildongmu.common.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorData(
         String errorCode,
         String field,
