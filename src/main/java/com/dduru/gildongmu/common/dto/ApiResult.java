0a8c2360962a83513ac7d803ec810c7afecb553f
shinheekim <dev.shinheekim@gmail.com>
2025-12-09 18:02:29 +0900

    [#111] chore(api): ApiResult에서 message 제외

--- src/main/java/com/dduru/gildongmu/common/dto/ApiResult.java
+++ src/main/java/com/dduru/gildongmu/common/dto/ApiResult.java
@@ -2,18 +2,13 @@ package com.dduru.gildongmu.common.dto;
 
 public record ApiResult<T>(
         Integer status,
-        String message,
         T data
 ) {
     public static <T> ApiResult<T> success(T data) {
-        return new ApiResult<>(200, "OK", data);
+        return new ApiResult<>(200, data);
     }
 
-    public static <T> ApiResult<T> success(String message, T data) {
-        return new ApiResult<>(200, message, data);
-    }
-
-    public static <T> ApiResult<T> success(Integer status, String message, T data) {
-        return new ApiResult<>(status, message, data);
+    public static <T> ApiResult<T> success(Integer status, T data) {
+        return new ApiResult<>(status, data);
     }
 }
