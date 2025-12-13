5e86a5b21647837374f161e363ded6efe86d4317
shinheekim <dev.shinheekim@gmail.com>
2025-12-13 21:08:09 +0900

    [#119] chore(user): 연속 공백 불가 검증 중 중복된 로직 제거

--- src/main/java/com/dduru/gildongmu/user/validator/NicknameConstraintValidator.java
+++ src/main/java/com/dduru/gildongmu/user/validator/NicknameConstraintValidator.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * {@link ValidNickname} 어노테이션의 실제 검증 로직을 담당합니다.
+ * ex. 금칙어 검증 등
  * 기본 어노테이션(@NotBlank, @Size, @Pattern)으로 처리하기 어려운 검증을 수행합니다.
  */
 public class NicknameConstraintValidator implements ConstraintValidator<ValidNickname, String> {
@@ -15,12 +16,6 @@ public class NicknameConstraintValidator implements ConstraintValidator<ValidNic
 
     @Override
     public boolean isValid(String nickname, ConstraintValidatorContext context) {
-
-        if (nickname.contains("  ")) {
-            setCustomMessage(context, "닉네임에 연속된 공백을 사용할 수 없습니다.");
-            return false;
-        }
-
         for (String badWord : BAD_WORDS) {
             if (nickname.contains(badWord)) {
                 setCustomMessage(context, "닉네임에 부적절한 단어가 포함되어 있습니다.");
