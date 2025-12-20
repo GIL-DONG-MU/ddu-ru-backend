8b1d623687a6bafae6bc52995f8f61bfefa41f3c
shinheekim <dev.shinheekim@gmail.com>
2025-12-20 21:29:35 +0900

    [#107] refactor(user): User 엔티티에서 프로필 관련 필드 제거
    - User 엔티티에서 nickname, profileImage, gender, phoneNumber, birthday 제거
    - User와 Profile을 OneToOne 관계로 연결 (mappedBy 사용)
    - UserRepository에서 existsByNickname, existsByPhoneNumber 제거

--- src/main/java/com/dduru/gildongmu/user/domain/User.java
+++ src/main/java/com/dduru/gildongmu/user/domain/User.java
@@ -1,16 +1,14 @@
 package com.dduru.gildongmu.user.domain;
 
-import com.dduru.gildongmu.user.enums.Gender;
+import com.dduru.gildongmu.common.entity.BaseTimeEntity;
+import com.dduru.gildongmu.profile.domain.Profile;
 import com.dduru.gildongmu.user.enums.OauthType;
 import com.dduru.gildongmu.user.enums.Role;
-import com.dduru.gildongmu.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
@@ -27,12 +25,6 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false, length = 50)
     private String name;
 
-    @Column(length = 14, unique = true)
-    private String nickname;
-
-    @Column(name = "profile_image", nullable = false, length = 500)
-    private String profileImage;
-
     @Column(name = "oauth_id", nullable = false, length = 100)
     private String oauthId;
 
@@ -44,49 +36,15 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private Role role = Role.USER;
 
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-
-    // AgeRange는 사용하지 않으므로 주석 처리
-    // @Enumerated(EnumType.STRING)
-    // @Column(name = "age_range", nullable = true)
-    // private AgeRange ageRange;
-
-    @Column(name = "phone_number", length = 20)
-    private String phoneNumber;
-
-    @Column(name = "birthday")
-    private LocalDate birthday;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Profile profile;
 
     @Builder
-    public User(String email, String name, String nickname, String profileImage, String oauthId,
-                OauthType oauthType, Role role, Gender gender, /* AgeRange ageRange, */ String phoneNumber, LocalDate birthday) {
+    public User(String email, String name, String oauthId, OauthType oauthType, Role role) {
         this.email = email;
         this.name = name;
-        this.nickname = nickname;
-        this.profileImage = profileImage;
         this.oauthId = oauthId;
         this.oauthType = oauthType;
         this.role = role != null ? role : Role.USER;
-        this.gender = gender;
-        // this.ageRange = ageRange;
-        this.phoneNumber = phoneNumber;
-        this.birthday = birthday;
     }
-
-    public void updateNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-//    public void updateAdditionalInfo(Gender gender, LocalDate birthday, String phoneNumber) {
-//        if (gender != null) {
-//            this.gender = gender;
-//        }
-//        if (birthday != null) {
-//            this.birthday = birthday;
-//        }
-//        if (phoneNumber != null) {
-//            this.phoneNumber = phoneNumber;
-//        }
-//    }
 }
