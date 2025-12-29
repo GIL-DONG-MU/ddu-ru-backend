2816dcf8b0d646b5e335667577515f691393e2fe
shinheekim <dev.shinheekim@gmail.com>
2025-12-30 01:33:08 +0900

    [#107] chore(profile): 프로필 최초 저장 request 유효성 추가

--- src/main/java/com/dduru/gildongmu/profile/dto/ProfileSetupRequest.java
+++ src/main/java/com/dduru/gildongmu/profile/dto/ProfileSetupRequest.java
@@ -1,13 +1,24 @@
 package com.dduru.gildongmu.profile.dto;
 
 import com.dduru.gildongmu.profile.validator.ValidNickname;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ProfileSetupRequest(
         @ValidNickname
+        @NotBlank
         String nickname,
+        @NotBlank
+        @Pattern(regexp = "^(M|F)$", message = "성별 형식이 올바르지 않습니다.")
         String gender,
+        @NotBlank
+        @Size(min = 11, max = 11, message = "전화번호 형식이 올바르지 않습니다.")
         String phoneNumber,
+        @NotBlank
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "생년월일 형식이 올바르지 않습니다. (예: 2000-01-01)")
         String birthday,
+        @NotBlank
         String verificationToken
 ) {
 }
