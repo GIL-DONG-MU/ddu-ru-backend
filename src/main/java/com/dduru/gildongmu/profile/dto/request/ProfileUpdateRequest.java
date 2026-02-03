eb0f389b22ace4264be6b4a6e93a1b195fa0411a
shinheekim <dev.shinheekim@gmail.com>
2026-02-03 15:51:46 +0900

    [#137] chore(profile): DTO에서 bgColorId null 방지

--- src/main/java/com/dduru/gildongmu/profile/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/dduru/gildongmu/profile/dto/request/ProfileUpdateRequest.java
@@ -2,6 +2,7 @@ package com.dduru.gildongmu.profile.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -13,6 +14,7 @@ public record ProfileUpdateRequest(
         @Pattern(regexp = "DEFAULT|AVATAR|UPLOADED", message = "프로필 이미지 타입은 DEFAULT, AVATAR, UPLOADED 중 하나여야 합니다.")
         String profileImageType,
 
+        @NotNull
         Long bgColorId,
 
         @Size(max = 60)
