5fab64442cecac61fb3256ae561dfa652eac1ee3
kimjaejoong <wcn07003@naver.com>
2026-02-03 15:07:04 +0900

    [#138] refactor(auth): 사용하지 않는 코드 제거

--- src/main/java/com/dduru/gildongmu/auth/dto/google/PersonExtendedInfo.java
+++
@@ -1,9 +0,0 @@
-//package com.dduru.gildongmu.auth.dto.google;
-//
-// 구글 People API 사용하지 않으므로 주석 처리
-//public record PersonExtendedInfo(String gender, String ageRange, String phoneNumber) {
-//
-//     public static PersonExtendedInfo empty() {
-//         return new PersonExtendedInfo(null, null, null);
-//     }
-//}
