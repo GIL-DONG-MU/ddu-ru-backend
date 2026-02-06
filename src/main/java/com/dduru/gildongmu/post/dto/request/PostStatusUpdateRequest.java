2b22aad0094fc8dfd0362f7fbb3ff9949781c873
kimjaejoong <wcn07003@naver.com>
2026-02-06 15:03:07 +0900

    [#134] refactor(post): boolean -> Boolean으로 변경

--- src/main/java/com/dduru/gildongmu/post/dto/request/PostStatusUpdateRequest.java
+++ src/main/java/com/dduru/gildongmu/post/dto/request/PostStatusUpdateRequest.java
@@ -3,7 +3,7 @@ package com.dduru.gildongmu.post.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record PostStatusUpdateRequest(
-    @NotNull(message = "모집 상태는 필수입니다.")
-    boolean open
+        @NotNull(message = "모집 상태는 필수입니다.")
+        Boolean open
 ) {
 }
