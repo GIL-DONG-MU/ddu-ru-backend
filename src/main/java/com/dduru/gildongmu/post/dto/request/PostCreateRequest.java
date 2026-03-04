8f7131b7164566f6ce331cd304387c12df1cc696
kimjaejoong <wcn07003@naver.com>
2026-03-04 10:06:03 +0900

    [#155] feat(post): 선호 연령대 필수

--- src/main/java/com/dduru/gildongmu/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/dduru/gildongmu/post/dto/request/PostCreateRequest.java
@@ -41,6 +41,7 @@ public record PostCreateRequest(
         @NotNull(message = "선호하는 성별을 골라주세요")
         Gender preferredGender,
 
+        @NotNull(message = "선호 연령대는 필수입니다")
         @Size(max = 3, message = "선호 연령대는 최대 3개까지 선택할 수 있습니다")
         List<AgeRange> preferredAges,
 
