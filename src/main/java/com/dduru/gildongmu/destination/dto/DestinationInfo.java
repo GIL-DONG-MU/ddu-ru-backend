e10f5f06d4b50bdf92b7cd373c3d4dae4f67f49b
kimjaejoong <wcn07003@naver.com>
2026-03-05 22:18:21 +0900

    [#155] feat(post): 여행지 리스트 추가 및 region 제거

--- src/main/java/com/dduru/gildongmu/destination/dto/DestinationInfo.java
+++ src/main/java/com/dduru/gildongmu/destination/dto/DestinationInfo.java
@@ -9,7 +9,6 @@ public record DestinationInfo(
         String countryCode,
         String countryName,
         String city,
-        String region,
         String image
 ) {
     public static DestinationInfo from(Destination destination) {
@@ -18,7 +17,6 @@ public record DestinationInfo(
                 .countryCode(destination.getCountryCode())
                 .countryName(destination.getCountryName())
                 .city(destination.getCity())
-                .region(destination.getRegion())
                 .image(destination.getImage())
                 .build();
     }
