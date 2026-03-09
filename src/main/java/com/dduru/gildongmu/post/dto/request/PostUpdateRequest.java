193eb3868b13fb18517e8df37a54b597bd9f74b4
kimjaejoong <wcn07003@naver.com>
2026-03-09 10:51:24 +0900

    [#167] feat(post): 사진 업로드 1장

--- src/main/java/com/dduru/gildongmu/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/dduru/gildongmu/post/dto/request/PostUpdateRequest.java
@@ -37,7 +37,7 @@ public record PostUpdateRequest(
         @Size(max = 2, message = "동행 연령대는 최대 2개까지 선택할 수 있습니다")
         List<AgeRange> preferredAges,
 
-        @Size(max = 5, message = "사진은 최대 5장까지 업로드 가능합니다")
+        @Size(max = 1, message = "사진은 1장만 업로드 가능합니다")
         List<String> photoUrls,
 
         @Size(max = 10, message = "태그는 최대 10개까지 가능합니다")
