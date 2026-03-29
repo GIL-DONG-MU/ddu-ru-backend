32dc0c81067fb4b73131246caac356ae590e8b6f
shinheekim <dev.shinheekim@gmail.com>
2026-03-29 15:39:46 +0900

    [#145] chore(chat): 불필요한 요청 dto 삭제

--- src/main/java/com/dduru/gildongmu/chat/dto/request/PrivateChatRoomCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dduru.gildongmu.chat.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record PrivateChatRoomCreateRequest(
-        @NotNull(message = "게시글 ID는 필수입니다.")
-        Long postId
-) {
-}
