c780ac41be645018774f1b7d28b5632f878537a2
shinheekim <128473266+shinheekim@users.noreply.github.com>
2026-03-31 10:44:30 +0900

    [#145] config(ws): Websocket 의존성 및 ws 엔드포인트 권한 추가

--- src/main/java/com/dduru/gildongmu/common/config/SecurityConfig.java
+++ src/main/java/com/dduru/gildongmu/common/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                         /* API 권한 설정 */
                         .requestMatchers("/api/v1/auth/logout").authenticated()
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         // 게시글 목록 / 상세 조회만 비로그인 허용
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts", "/api/v1/posts/*").permitAll()
                         .requestMatchers("/api/v1/verifications/**").authenticated()
