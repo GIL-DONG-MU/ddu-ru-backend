92fb13ff0d3876281f87326525fd0c9ed941397d
kimjaejoong <wcn07003@naver.com>
2026-06-17 11:00:24 +0900

    [#271] fix(post): request에서 size 복ㄱ

--- src/main/java/com/dduru/gildongmu/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/dduru/gildongmu/post/dto/request/PostUpdateRequest.java
@@ -10,8 +10,10 @@ import java.util.List;
 public record PostUpdateRequest(
         Long destinationId,
 
+        @Size(min = 5, max = 40, message = "제목은 5자 이상 40자 이하여야 합니다")
         String title,
 
+        @Size(min = 20, max = 1000, message = "내용은 20자 이상 1000자 이하여야 합니다")
         String content,
 
         @FutureOrPresent(message = "여행 시작일은 오늘 이후여야 합니다")
