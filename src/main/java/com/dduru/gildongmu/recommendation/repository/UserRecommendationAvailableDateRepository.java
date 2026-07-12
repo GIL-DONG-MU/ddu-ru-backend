1721ab3a580b7e2cd1faf35cb1371873f12bddd5
kimjaejoong <wcn07003@naver.com>
2026-07-12 19:27:56 +0900

    [#315] feat(mypage): 여행 선호 설정 조회/삭제 쿼리 추가

--- src/main/java/com/dduru/gildongmu/recommendation/repository/UserRecommendationAvailableDateRepository.java
+++ src/main/java/com/dduru/gildongmu/recommendation/repository/UserRecommendationAvailableDateRepository.java
@@ -2,9 +2,16 @@ package com.dduru.gildongmu.recommendation.repository;
 
 import com.dduru.gildongmu.recommendation.domain.UserRecommendationAvailableDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRecommendationAvailableDateRepository extends JpaRepository<UserRecommendationAvailableDate, Long> {
     List<UserRecommendationAvailableDate> findAllByUser_Id(Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM UserRecommendationAvailableDate d WHERE d.user.id = :userId")
+    void deleteAllByUserId(@Param("userId") Long userId);
 }
